fe2c8f06ff0e9eb219c5e9d4d515678b035b1566
Johan Stenehall <stenehall@gmail.com>
2016-01-27 16:24:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+Narrative moment widget
+===
+
+This widget lets you display a Narrative moment on your own site.
+
+Setup
+---
+
+First you need to register a Narrative Open Platform app.
+
+1. Sign in to [open.getnarrative.com](http://open.getnarrative.com/) and [Create a New App](http://open.getnarrative.com/apps)
+2. Add your sites domain to the Redirect URIs, this will also white list this domain in CORS.
+3. Copy your apps Client ID and Client secret
+4. OBS! There's currently a limit to new Open Platform apps restricting them from doing so called anonymous api calls, to enable this please email [open@getnarrative.com](mailto://open@getnarrative.com) and ask to get your app enabled.
+
+Usage
+---
+
+Grap the UUID for the moment you want to display, it's the last part of a shared moment link. Looking something like this `4f0c241562a24bce86a9e24a009c845d`
+
+
+Then add both the jquery plugin and the css file to your site. Lastly add the following to your html.
+
+```javascript
+var clientID = '<Your Client ID>';
+var clientSecret = '<Your Client Secret>';
+var momentUUID = '<The moments UUID you want to display, needs to be public>';
+$('#nrtv__current-photo').nrtvScrubb(momentUUID, clientID, clientSecret);
+```
+
+And this HTML code
+
+```html
+<div id="nrtv__current-photo" class="nrtv__loading">
+  <div class="nrtv__progress"></div>
+  <div class="nrtv__meta"></div>
+</div>
+```
---
+++ src/jquery-nrtv-moment-widget.js
@@ -0,0 +1,171 @@
+(function( $ ) {
+  var nrtvElement;
+  $.fn.nrtvScrubb = function(uuid, clientID, clientSecret) {
+    nrtvElement = this
+      $(document).ready(function() {
+        var scrubb = new nrtvScrubb(uuid, clientID, clientSecret);
+      });
+
+    /**
+     * Nrtv base class
+     *
+     * Handles all communication with the Narrative api.
+     *
+     * @params {clientId} Your client_id for this application
+     * @params {clientSecret} Your client_secret for this application
+     */
+    function Nrtv(clientID, clientSecret) {
+      var self = this;
+      self.cookieName = 'nrtv-codepen';
+      self.apiUrl = 'https://narrativeapp.com/';
+    }
+
+    /**
+     * Get an anonymous access token from the Api
+     *
+     * If we have an existing api token use that, else look for one the cookie,
+     * lastly request a new one.
+     *
+     * @params {callback} - Callback to be run once we've fetched the moment
+     */
+    Nrtv.prototype.getAnonymousToken = function(callback) {
+      var self = this;
+
+      if(self.token) {
+        return callback(self.token);
+      }
+
+      var cookie;
+      var parts = ('; ' + document.cookie).split('; '+self.cookieName+'=');
+      if (parts.length == 2) {
+        self.token = JSON.parse(parts.pop().split(';').shift());
+        return callback(self.token);
+      }
+
+      var url = self.apiUrl + 'oauth2/token/?' + $.param({grant_type: 'client_credentials'});
+      $.ajax({
+        url: url,
+        method: 'post',
+        beforeSend: function(request) {
+          request.setRequestHeader('Authorization', 'Basic ' + btoa(clientID+':'+clientSecret));
+        }
+      }).done(function(token) {
+        var date = new Date();
+        date.setTime(date.getTime()+(1*24*60*60*1000));
+
+        document.cookie = self.cookieName+'='+JSON.stringify(token)+'; expires='+date.toGMTString()+'; path=/';
+        self.token = token;
+        return callback(token);
+      });
+    };
+
+    /**
+     * Api function
+     *
+     * First get an anonymous token, then do an api call.
+     *
+     * @params {string} - Endpoint to call
+     * @params {callback} - Callback to be run once we have a response
+     */
+    Nrtv.prototype.api = function(path, callback) {
+      var self = this;
+      self.getAnonymousToken(function(token) {
+        $.ajax({
+          url: self.apiUrl + '/api/v2' + path,
+          beforeSend: function(request) {
+            request.setRequestHeader('Authorization', 'Bearer '+token.access_token);
+          }
+        }).done(function(data) {
+          callback(data);
+        });
+      });
+    };
+
+    /**
+     * Fetch a moments all photos
+     *
+     * @params {uuid} - The unique identifier of the moment
+     * @params {callback} - Callback to be run once we've fetched the photos
+     */
+    Nrtv.prototype.getPhotos = function(uuid, callback) {
+      this.api('/moments/'+uuid+'/photos/?limit=9999', function(data) {
+        callback(data.results);
+      });
+    };
+
+    /**
+     * Fetch the moments meta information
+     *
+     * @params {uuid} - The unique identifier of the moment
+     * @params {callback} - Callback to be run once we've fetched the moment
+     */
+    Nrtv.prototype.getMoment = function(uuid, callback) {
+      this.api('/moments/'+uuid+'/', function(data) {
+        callback(data);
+      });
+    };
+
+    /**
+     * NrtvScrubb class
+     *
+     */
+    function nrtvScrubb(uuid, clientID, clientSecret) {
+      var self = this;
+      var nrtv = new Nrtv(clientID, clientSecret);
+      var width = nrtvElement.width();
+      self.momentPhotos = [];
+      $(document).resize(function() {
+        width = nrtvElement.width();
+      });
+
+      nrtv.getMoment(uuid, function(data) {
+        $('.nrtv__meta', nrtvElement).html('<a href="https://getnarrative.com/shared/moment/'+data.uuid+'"><b>'+data.caption+'</b></a>');
+      });
+
+      nrtv.getPhotos(uuid, function(response) {
+        var photos = [];
+        $.each(response, function(key, photo) {
+          photos.push(photo.renders.g1_smartphone.url);
+        });
+
+        $(nrtvElement).mousemove(function (event) {
+          var procent = Math.round(event.pageX / width * 1000) / 10;
+          var photoIndex = Math.round((photos.length-1) * (procent / 100));
+          self.updateScrubb(photoIndex, photos);
+        });
+
+        self.updateScrubb(0, photos);
+        self.preloadPhoto(photos);
+        nrtvElement.removeClass('nrtv__loading');
+      });
+    }
+
+    /**
+     * Preload all photos as fast as we can.
+     *
+     * This will decreases the flickering that happens if a photo isn't loaded
+     * when tried to view.
+     *
+     * @params {array} Array of photos to preload
+     */
+    nrtvScrubb.prototype.preloadPhoto = function(photos) {
+      var images = [];
+      for (i = 0; i < photos.length; i++) {
+        images[i] = new Image();
+        images[i].src = photos[i];
+      }
+    };
+
+    /**
+     * Update the DOM element
+     *
+     * @params {Int} Index of the selected photo
+     * @params {Array} Array of photos.
+     */
+    nrtvScrubb.prototype.updateScrubb = function(photoIndex, photos) {
+      nrtvElement.css({'background-image': 'url('+photos[photoIndex]+')' });
+      $('.nrtv__progress', nrtvElement).css({'width': ((photoIndex/photos.length)*100)+'%'});
+    };
+
+  }
+}( jQuery ));
